b8a9b567e66cfe980e460fd75f9d099ebc0739b9
Iftikher Lutfur <iftikherlutfur@gmail.com>
2025-02-06 15:26:38 +0600

    Install and setup  the next.js project

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -19,9 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>

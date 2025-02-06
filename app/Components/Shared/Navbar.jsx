f1bdf73b01aacf32476e0afb7abc534e134b2eb1
Iftikher Lutfur <iftikherlutfur@gmail.com>
2025-02-06 15:49:00 +0600

    Added the navbar

--- app/Components/Shared/Navbar.jsx
+++ app/Components/Shared/Navbar.jsx
@@ -4,7 +4,13 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            
+            <ul className='text-xl justify-between px-10 z-20 absolute w-full text-white bg-zinc-800 uppercase flex items-center py-3'>
+                <li>Home</li>
+                <li>our menus</li>
+                <li>Order online</li>
+                <li>about us</li>
+                <li>Contact us</li>
+            </ul>
         </div>
     );
 };
